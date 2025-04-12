396893c9037e45c3370d54352069bf5b6d7b875b
Monika G <monikaggopalappa@gmail.com>
2025-04-12 21:08:34 +0530

    https://github.com/monika047/april2

--- factno.c
+++ factno.c
@@ -4,4 +4,9 @@ int main() {
     unsigned long long fact = 1;
     printf("Enter an integer: ");
     scanf("%d", &n);
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
 
